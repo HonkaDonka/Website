cf1f7636110e7ea191ace60bd4e260495c97610b
HonkaDonka <vinsonh789@gmail.com>
2023-08-17 22:26:38 +0000

    about me

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-var navLinks = document.getElementById("navLinks");
+var tablinks = document.getElementsByClassName("tab-link");
+var tabcontent = document.getElementsByClassName("tab-content");
 
-// Menu opening & closing functions
+function openTab(tabname) {
+  for(i of tablinks) {
+    i.classList.remove("active-link");
+  }
 
-function openMenu() {
-  navLinks.style.right = "0";
+  for(i of tabcontent) {
+    i.classList.remove("active-tab");
+  }
+
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
 }
-
-function closeMenu() {
-  navLinks.style.right = "-200px";
-}
-
